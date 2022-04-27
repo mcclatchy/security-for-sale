bc3eba07e0c9676b788be77a087029ef18ef2630
David Newcomb <dnewcomb@mcclatchy.com>
2022-04-27 07:16:10 -0700

    Switching masthead order, adding acknowledgments fully

--- src/js/components/SimpleMasthead.svelte
+++ src/js/components/SimpleMasthead.svelte
@@ -6,12 +6,12 @@ const logoAltText = window.pageInfo["marketInfo.logoAltText"];
 </script>
 
 <div class="simple-flag">
-    <a href="https://www.newsobserver.com" class="market-logo" aria-label="News & Observer">
-        <img id="news-logo" src="https://newsobserver.com/wps/build/images/newsobserver/logo.svg" alt="News & Observer Logo">
+    <a href="https://www.charlotteobserver.com" class="market-logo" aria-label="Charlotte Observer">
+        <img id="charlotte-logo" class="filtered-logo" src="https://charlotteobserver.com/wps/build/images/charlotteobserver/logo.svg" alt="Charlotte Observer Logo">
     </a>
     <div class="vertical-divider"/>
-    <a href="https://www.charlotteobserver.com" class="market-logo" aria-label="Charlotte Observer">
-        <img id="charlotte-logo" src="https://charlotteobserver.com/wps/build/images/charlotteobserver/logo.svg" alt="Charlotte Observer Logo">
+    <a href="https://www.newsobserver.com" class="market-logo" aria-label="News & Observer">
+        <img id="news-logo" class="filtered-logo"src="https://newsobserver.com/wps/build/images/newsobserver/logo.svg" alt="News & Observer Logo">
     </a>
 </div>
 
@@ -46,7 +46,7 @@ const logoAltText = window.pageInfo["marketInfo.logoAltText"];
   display: inline-block;
   height: 30px;
 }
-.simple-flag img {
+.filtered-logo {
   opacity: 1;
   height: inherit;
   filter: brightness(0%) invert(29%) sepia(75%) saturate(1957%) hue-rotate(330deg) brightness(90%) contrast(88%);
